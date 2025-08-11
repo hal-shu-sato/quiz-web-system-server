6de508b658bd986d28971a3f749aabb8909c09d2
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-08-12 01:25:32 +0900

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { ignores: ['dist'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['*.mjs'],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintConfigPrettier,
+);
